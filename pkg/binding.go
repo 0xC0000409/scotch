2c931c24206b3d0bdb2f74631093070bb56ebe61
0xC0000409 <zuka.gergaia@gmail.com>
2023-05-02 13:31:50 +0400

    Handle binding errors gracefully in "BindJsonPretty"

--- pkg/binding.go
+++ pkg/binding.go
@@ -3,6 +3,8 @@ package pkg
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"net/http"
 )
 
 type UriIntId struct {
@@ -11,8 +13,13 @@ type UriIntId struct {
 
 func BindJsonPretty[T any](c *gin.Context, form T) error {
 	if err := c.ShouldBindBodyWith(&form, binding.JSON); err != nil {
-		c.Error(err).SetType(gin.ErrorTypeBind)
-		prettyValidationErrors(c)
+		err, ok := err.(validator.ValidationErrors)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"Error": "Bad Request"})
+		} else {
+			c.Error(err).SetType(gin.ErrorTypeBind)
+			prettyValidationErrors(c)
+		}
 		return err
 	}
 	return nil
